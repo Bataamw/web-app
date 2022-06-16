41d0aabadd234142449d7e985e69102a63a35720
Bataamw <Bataamw@gmail.com>
2022-06-16 09:14:16 +0800

    sticky header duusav

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -16,7 +16,6 @@ class RevealOnScroll {
     window.addEventListener(
       "resize",
       debounce(() => {
-        console.log("sdasd");
         this.browserHeight = window.innerHeight;
       }, 300)
     );
